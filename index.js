0a27a58be77e892bae49a25ee9f898fc664cd006
Krupa Dulobo <115632825+Thisaintkrupaa@users.noreply.github.com>
2024-01-19 13:54:44 +0000

    Update index.js

--- index.js
+++ index.js
@@ -23,29 +23,55 @@ message: 'provide a description of your Project: ',
 { 
     type: 'input',
 name: 'installation',
-message: 'Enter installation instructions: ',
+message: 'what command should be used to run dependencies',
+default: 'npm test'
+},
+
+{ 
+    type: 'input',
+name: 'usage',
+message: 'What should the user know about using the repo?',
 },
 
 { 
     type: 'list',
 name: 'license',
 message: 'choose a license for your project: ',
-choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None']
+choices: ['MIT', 'APACHE ', 'GPL', 'BSD', 'None']
+},
+
+{ 
+    type: 'input',
+name: 'contribution',
+message: 'What does the user know about contributing to the repo?',
 },
 
 
 { 
     type: 'input',
-name: 'title',
-message: 'Enter the title of your project: ',
-}
+name: 'testscommand',
+message: 'what command should be used to run tests',
+default: 'npm i'
+},
+
+{ 
+    type: 'input',
+name: 'github',
+message: 'Enter your Github Username: ',
+},
+
+{ 
+    type: 'input',
+name: 'email',
+message: 'Enter your Email Address: ',
+},
 
 ];
 
 // function to write README file 
 function writeToFile(fileName, data) {
     fs.writeFile(fileName, data , (err) => err ? console.log(err) : console.log('Generating ReadMe.../Succesfully Generated!!!'));
-}
+};
      
 
 // function to initialize program
@@ -65,3 +91,4 @@ function init() {
 
 // function call to initialize program
 init();
+
